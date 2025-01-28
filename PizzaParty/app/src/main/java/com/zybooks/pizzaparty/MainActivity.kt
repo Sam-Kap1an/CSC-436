d834bff82c2e986ce77217b2091f148a8153b0cb
sfkaplan <sfkaplan@calpoly.edu>
2025-01-27 16:29:07 -0800

    fixes

--- PizzaParty/app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ PizzaParty/app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -3,28 +3,19 @@ package com.zybooks.pizzaparty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
 import androidx.compose.foundation.layout.fillMaxSize
-
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-
-import androidx.compose.runtime.Composable
-
 import androidx.compose.ui.Modifier
-
-import androidx.compose.ui.tooling.preview.Preview
-
 import com.zybooks.pizzaparty.ui.PizzaPartyScreen
 import com.zybooks.pizzaparty.ui.theme.PizzaPartyTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            PizzaPartyTheme(dynamicColor = false) {
+            PizzaPartyTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -36,11 +27,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PizzaPartyTheme {
-        PizzaPartyScreen()
-    }
-}
